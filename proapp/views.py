ca2268200fe985f1fe4a4b8953c5f563156e73c1
mdkashifsidd2020 <142329771+mdkashifsidd2020@users.noreply.github.com>
2023-08-30 15:54:35 +0530

    first commit

---
+++ proapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from .models import *
+
+# Create your views here.
+def index(request):
+    if request.method == 'POST':
+        name=request.POST['name']
+        email=request.POST['email']
+        subject=request.POST['subject']
+        message=request.POST['message']
+        mydata=dform(name='name',email='email',subject='subject',message='message')
+        mydata.save()
+    return render(request , "index.html")
